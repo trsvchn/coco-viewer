ec91af0827675e8649f1589f6b99a1a2116ae5ba
Taras Savchyn <savchyntm@gmail.com>
2019-04-08 19:44:42 +0300

    Add argparse

--- cocoviewer.py
+++ cocoviewer.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import json
 import tkinter as tk
@@ -8,27 +9,30 @@ from PIL import Image, ImageDraw, ImageTk
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
-
-TEST_IMAGES = 'val2017'
-TEST_ANNOTATIONS = 'annotations/instances_val2017.json'
-
+parser = argparse.ArgumentParser(description='View images with bboxes from COCO dataset')
+parser.add_argument('-i', '--images', default='', type=str, metavar='PATH', help='path to image folder')
+parser.add_argument('-a', '--annotations', default='', type=str, metavar='PATH', help='path to annotations json file')
 
 class App(tk.Tk):
     """Main App class"""
 
-    def __init__(self, path_to_images: str,
+    def __init__(self,
+                 path_to_images: str,
                  path_to_gt_anns: str,
                  path_to_pred_anns: str = None) -> None:
         super().__init__()
-
         self.path_to_images = path_to_images
         self.path_to_gt_anns = path_to_gt_anns
         self.path_to_pred_anns = path_to_pred_anns
         self.image = None
+
         self.print_debug()
+
         self.image_list = self.anns_parser()
+
         self.current_image = self.image_list.next()
         self.load_image(self.current_image, start=True)
+
         self.bind("<Left>", self.previous_image)
         self.bind("<Right>", self.next_image)
         # TODO: ADD exit button
@@ -118,8 +122,8 @@ class ImageList:
 
 def main():
     """Runs app"""
-    # TODO: add argparse
-    app = App(TEST_IMAGES, TEST_ANNOTATIONS)
+    args = parser.parse_args()
+    app = App(args.images, args.annotations)
     app.mainloop()
 
 
