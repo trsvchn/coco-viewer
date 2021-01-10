94a3fd24c91e897a5002dd3d1c18bb877e9dc1fb
Taras Savchyn <trsvchn@gmail.com>
2021-01-10 16:01:02 +0200

    Refactor the app structure (again)

--- cocoviewer.py
+++ cocoviewer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """COCO Dataset Viewer.
 
 View images with bboxes from the COCO dataset.
@@ -17,133 +16,66 @@ from PIL import Image, ImageDraw, ImageTk
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
-
 parser = argparse.ArgumentParser(description="View images with bboxes from the COCO dataset")
 parser.add_argument("-i", "--images", default='', type=str, metavar="PATH", help="path to images folder")
 parser.add_argument("-a", "--annotations", default='', type=str, metavar="PATH", help="path to annotations json file")
 
 
-class App(tk.Tk):
-    """Main App class.
+class ImageWidget:
+    """Main Image Widget that displays composed image.
     """
-    # TODO: labels for classes
-    # TODO: predicted bboxes drawing (from custom models).
-    def __init__(self,
-                 path_to_images: str,
-                 path_to_gt_anns: str,
-                 path_to_pred_anns: str = None) -> None:
-        super().__init__()
-        # for the debugging
-        self.print_debug()
-
-        self.title("COCO Viewer")
-        self.init_menu()
-
-        self.path_to_images = path_to_images
-        self.path_to_gt_anns = path_to_gt_anns
-        self.path_to_pred_anns = path_to_pred_anns
-        self.image = None
-
-        self.instances = self.load_annotations()
-        self.images = ImageList(self.get_images())  # NOTE: image list is based on annotations file
-        self.categories = self.get_categories()
-
-        self.current_image = None
+    # TODO: labels for object classes
+    # TODO: predicted bboxes drawing (from models)
+    def __init__(self, parent, image_dir, annotations_file) -> None:
+        self.parent = parent
+        self.image_dir = image_dir
+        instances, images, categories = parse_coco(annotations_file)
+        self.instances = instances
+        self.images = ImageList(images)  # NOTE: image list is based on annotations file
+        self.categories = categories
+        self.current_image = self.images.next()  # Set the first image as current
         self.composed_img = None  # To store composed PIL Image
-
-        # Init Image Widget
-        self.init_image()
-        # Bind events, shortcuts
-        self.bind_events()
-
-    def bind_events(self):
-        self.bind("<Left>", self.previous_image)
-        self.bind("<k>", self.previous_image)
-        self.bind("<Right>", self.next_image)
-        self.bind("<j>", self.next_image)
-        self.bind("<Control-q>", self.exit)
-        self.bind("<Control-w>", self.exit)
-        self.bind("<Control-s>", self.save_image)
-
-    def init_menu(self):
-        menu_bar = tk.Menu(self, )
-        file_menu = tk.Menu(menu_bar, tearoff=0)
-        file_menu.add_command(label="Save", accelerator="Ctrl+S", command=self.save_image)
-        file_menu.add_separator()
-        file_menu.add_command(label="Exit", accelerator="Ctrl+Q", command=self.destroy)
-        menu_bar.add_cascade(label="File", menu=file_menu)
-        self.config(menu=menu_bar)
+        # Load the very first image
+        self.load_image(self.current_image)
+        img = ImageTk.PhotoImage(self.composed_img)
+        # Init the image widget
+        self.image = tk.Label(self.parent, image=img)
+        self.image.pack()
+        self.image.image = img
 
     def exit(self, event):
         if event:
-            self.destroy()
-
-    def load_annotations(self) -> dict:
-        """Loads annotations file.
-        """
-        if '.json' in self.path_to_gt_anns:
-            logging.info('Parsing json...')
-
-        with open(self.path_to_gt_anns) as f:
-            instances = json.load(f)
-        return instances
-
-    def get_images(self) -> list:
-        """Extracts all image ids and file names from annotations file.
-        """
-        return [(image['id'], image['file_name']) for image in self.instances['images']]
-
-    def get_objects(self, image_id: int) -> list:
-        """Extracts all object from annotations file for image with image_id.
-        """
-        return [obj for obj in self.instances['annotations'] if obj['image_id'] == image_id]
-
-    def get_categories(self) -> dict:
-        """Extracts categories from annotations file and prepares color for each one.
-        """
-        # Get some colors
-        hsv_tuples = [(x / 80, 1., 1.) for x in range(80)]
-        colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
-        colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))
-        random.seed(42)
-        random.shuffle(colors)
-        random.seed(None)
-
-        # Parse categories
-        categories = list(zip([[category['id'], category['name']] for category in self.instances['categories']], colors))
-        return dict([[cat[0][0], [cat[0][1], cat[1]]] for cat in categories])
+            self.parent.destroy()
 
     def load_image(self, image: tuple):
         """Loads image as PIL Image.
         """
         # TODO: function is too long
         img_id, img_name = image
-
-        full_path = os.path.join(self.path_to_images, img_name)
-
+        full_path = os.path.join(self.image_dir, img_name)
         # Open image
-        img_open = Image.open(full_path).convert('RGBA')
-
+        img_open = Image.open(full_path).convert("RGBA")
         # Create layer for bbox
-        bbox_layer = Image.new('RGBA', img_open.size, (255, 255, 255, 0))
+        bbox_layer = Image.new("RGBA", img_open.size, (255, 255, 255, 0))
         draw = ImageDraw.Draw(bbox_layer)
 
         # test bbox
         # draw.rectangle(xy=[300, 100, 500, 300], fill=(255, 0, 0, 80), outline=(255, 0, 0, 0))
 
-        objects = self.get_objects(img_id)
-        obj_categories = [self.categories[obj['category_id']] for obj in objects]
+        # Get objects
+        objects = [obj for obj in self.instances["annotations"] if obj["image_id"] == img_id]
+        obj_categories = [self.categories[obj["category_id"]] for obj in objects]
 
         # Extracting bbox coordinates
-        bboxes = [[obj['bbox'][0],
-                   obj['bbox'][1],
-                   obj['bbox'][0] + obj['bbox'][2],
-                   obj['bbox'][1] + obj['bbox'][3]] for obj in objects]
+        bboxes = [[obj["bbox"][0],
+                   obj["bbox"][1],
+                   obj["bbox"][0] + obj["bbox"][2],
+                   obj["bbox"][1] + obj["bbox"][3]] for obj in objects]
 
         # Extracting masks
         masks = True
         if masks:
-            masks = [obj['segmentation'] for obj in objects]
+            masks = [obj["segmentation"] for obj in objects]
 
             # draw masks
             for c, m in zip(obj_categories, masks):
@@ -181,23 +113,9 @@ class App(tk.Tk):
             filetypes=filetypes,
             defaultextension=defaultextension,
         )
-
-        # If not canceled
+        # If not canceled:
         if file:
             self.composed_img.save(file)
-
-    def init_image(self):
-        """Instantiates Image Label Widget.
-        """
-        # Set the first image as current
-        self.current_image = self.images.next()
-        # Load the very first image
-        self.load_image(self.current_image)
-        img = ImageTk.PhotoImage(self.composed_img)
-        # Init the image widget
-        self.image = tk.Label(self, image=img)
-        self.image.pack()
-        self.image.image = img
 
     def update_image(self):
         """Updates Image Label Widget.
@@ -206,9 +124,6 @@ class App(tk.Tk):
         # Update the image widget
         self.image.configure(image=img)
         self.image.image = img
-
-    def print_debug(self):
-        logging.info("Starting app...")
 
     def next_image(self, event):
         """Loads the next image in a list.
@@ -227,13 +142,80 @@ class App(tk.Tk):
             self.update_image()
 
 
+def parse_coco(annotations_file: str) -> tuple:
+    """Parses COCO json annotation file.
+    """
+    instances = load_annotations(annotations_file)
+    images = get_images(instances)
+    categories = get_categories(instances)
+    return instances, images, categories
+
+
+def load_annotations(fname: str) -> dict:
+    """Loads annotations file.
+    """
+    logging.info(f"Parsing {fname}...")
+
+    with open(fname) as f:
+        instances = json.load(f)
+    return instances
+
+
+def get_images(instances: dict) -> list:
+    """Extracts all image ids and file names from annotations file.
+    """
+    return [(image["id"], image["file_name"]) for image in instances["images"]]
+
+
+def get_categories(instances: dict) -> dict:
+    """Extracts categories from annotations file and prepares color for each one.
+    """
+    # Get some colors
+    hsv_tuples = [(x / 80, 1., 1.) for x in range(80)]
+    colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+    colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))
+    random.seed(42)
+    random.shuffle(colors)
+    random.seed(None)
+
+    # Parse categories
+    categories = list(zip([[category["id"], category["name"]] for category in instances["categories"]], colors))
+    categories = dict([[cat[0][0], [cat[0][1], cat[1]]] for cat in categories])
+    return categories
+
+
+def menu(root, image):
+    """Adds a Menu bar.
+    """
+    menu_bar = tk.Menu(root)
+    file_menu = tk.Menu(menu_bar, tearoff=0)
+    file_menu.add_command(label="Save", accelerator="Ctrl+S", command=image.save_image)
+
+    file_menu.add_separator()
+    file_menu.add_command(label="Exit", accelerator="Ctrl+Q", command=root.destroy)
+    menu_bar.add_cascade(label="File", menu=file_menu)
+    root.config(menu=menu_bar)
+
+
+def bind_events(root, image):
+    """Binds events.
+    """
+    root.bind("<Left>", image.previous_image)
+    root.bind("<k>", image.previous_image)
+    root.bind("<Right>", image.next_image)
+    root.bind("<j>", image.next_image)
+    root.bind("<Control-q>", image.exit)
+    root.bind("<Control-w>", image.exit)
+    root.bind("<Control-s>", image.save_image)
+
+
 class ImageList:
     """Handles iterating through the images.
     """
     def __init__(self, images: list):
-        self.image_list = images
+        self.image_list = images or []
         self.n = -1
-        self.max = len(images)
+        self.max = len(self.image_list)
 
     def next(self):
         """Sets the next image as current.
@@ -259,10 +241,19 @@ class ImageList:
         return current_image
 
 
+def print_info(message: str):
+    logging.info(message)
+
+
 def main():
+    print_info("Starting the app...")
     args = parser.parse_args()
-    app = App(args.images, args.annotations)
-    app.mainloop()
+    root = tk.Tk()
+    root.title("COCO Viewer")
+    image = ImageWidget(root, args.images, args.annotations)
+    menu(root, image)
+    bind_events(root, image)
+    root.mainloop()
 
 
 if __name__ == "__main__":
